a0e640092323176a1ce6f9b56cb088f2b6ac9902
JuMalIO <44319139+JuMalIO@users.noreply.github.com>
2021-09-10 00:27:58 +0300

    Create w25q_config.h

---
+++ w25q_config.h
@@ -0,0 +1,10 @@
+#ifndef __w25q_config_H
+#define __w25q_config_H
+
+#include "main.h"
+
+#define W25Q_SPI                   hspi3
+#define W25Q_CS_PORT               SPI3_CS_GPIO_Port
+#define W25Q_CS_PIN                SPI3_CS_Pin
+
+#endif /* __w25q_config_H */
