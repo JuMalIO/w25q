5ab9bcd3d7fb6a0bd2ed01ab3bf1dbc3aeea122b
JuMalIO <44319139+JuMalIO@users.noreply.github.com>
2021-09-10 22:51:36 +0300

    Update w25q.h

--- w25q.h
+++ w25q.h
@@ -4,6 +4,10 @@
 
 #include <stdint.h>
 
+#define W25Q_PAGE_SIZE        0x0100                   /* 256 B */
+#define W25Q_SECTOR_SIZE      0x1000                   /*  4KB */
+#define W25Q_BLOCK_SIZE       (W25Q_SECTOR_SIZE * 16)  /* 64KB */
+
 void W25Q_Init(void);
 uint32_t W25Q_ReadId(void);
 uint32_t ReadBlockCount(void);
